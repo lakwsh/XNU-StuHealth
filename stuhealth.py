2fb9edc6de064c213958cc702567d472c7dbb602
lakwsh <uitony@126.com>
2021-03-05 13:40:21 +0800

    Add files via upload

---
+++ stuhealth.py
@@ -0,0 +1,75 @@
+from time import strftime,localtime
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import algorithms
+from Crypto.Cipher import AES
+from random import randint
+from base64 import b64encode
+import requests
+from json import loads
+from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
+from email.utils import formataddr
+
+def pkcs7_padding(data):
+    padder = padding.PKCS7(algorithms.AES.block_size).padder()
+    padded_data = padder.update(data.encode('utf-8')) + padder.finalize()
+    return padded_data
+def encrypt(data):
+    key = 'xAt9Ye&SouxCJziN'.encode('utf-8')
+    cryptor = AES.new(key,AES.MODE_CBC,key)
+    return b64encode(cryptor.encrypt(pkcs7_padding(data))).decode().replace('/','_').replace('+','-')
+def req(u,d):
+    res = requests.post(u,json = d,headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+        'Referer': 'https://stuhealth.jnu.edu.cn/'
+    })
+    res.encoding = 'utf8'
+    if res.status_code == requests.codes.ok:
+        return loads(res.text)
+    raise RuntimeError('Req-Error: ' + str(res.status_code) + '\nText: ' + res.text)
+def clean(tb,l):
+    if not tb:
+        raise RuntimeError('Table-TypeError')
+    for key in list(tb):
+        if not tb[key] or key in l:
+            tb.pop(key)
+    return tb
+def mail(rec,msg):
+    message = MIMEText(msg,'plain','utf-8')
+    message['Subject'] = 'Stuhealth'
+    message['From'] = formataddr(['Script','发件人邮箱'])
+    try:
+        smtp = SMTP_SSL()
+        smtp.connect('邮箱服务器',465)
+        smtp.login('邮箱登录名','邮箱登录密码')
+        smtp.sendmail('发件人邮箱',rec,message.as_string())
+        smtp.quit()
+    except Exception as e:
+        print(msg)
+        print(str(e))
+        exit(1)
+
+ul = {'学号1':'密码1','学号2':'密码2',...}
+result = strftime('%Y-%m-%d %H:%M:%S',localtime())
+try:
+    for usr,pwd in ul.items():
+        ret = req('https://stuhealth.jnu.edu.cn/api/user/login',{'username':usr,'password':encrypt(pwd).replace('=','*',1)})
+        meta = ret['meta']
+        jid = ret['data']['jnuid']
+        result += '\n' + usr + ' => ' + meta['msg']
+        if meta['code'] == 200:
+            ret = req('https://stuhealth.jnu.edu.cn/api/user/stuinfo',{'jnuid':jid,'idType':ret['data']['idtype']})
+            data = ret['data']
+            mt = clean(data['mainTable'],['id','personType','createTime','del'])
+            mt['personName'] = data['xm']
+            mt['sex'] = data['xbm']
+            mt['professionName'] = data['zy']
+            mt['collegeName'] = data['yxsmc']
+            mt['declareTime'] = data['declare_time']
+            mt['temperature'] = '36.' + str(randint(1,7))
+            ret = req('https://stuhealth.jnu.edu.cn/api/write/main',{'mainTable':mt,'secondTable':clean(data['secondTable'],['id','mainId']),'jnuid':jid})
+            result += '\n' + ret['meta']['msg']
+            result += '\nTime => ' + ret['meta']['timestamp']
+    mail(['打卡成功邮箱'],result)
+except Exception as e:
+    mail(['脚本错误邮箱'],result + '\nLine-' + str(e.__traceback__.tb_lineno) + ': ' + str(e))
